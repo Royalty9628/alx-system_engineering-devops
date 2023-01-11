3a3cd2011448b9afe060f96e5ca90f10e244354e
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2023-01-11 04:02:51 -0800

    create pppet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file
+file { '/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
